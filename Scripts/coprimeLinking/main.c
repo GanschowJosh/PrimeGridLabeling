f8f1b9160e37b69514566363294d66207befcbdf
Miles Roberts <milesrobertsmain@gmail.com>
2024-10-21 13:59:45 -0500

    replaced static arrays with linked lists

--- Scripts/coprimeLinking/main.c
+++ Scripts/coprimeLinking/main.c
@@ -9,14 +9,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define COPRIME_ARRAY_LENGTH 8100
-
-typedef struct Node {
+typedef struct Root {
     int value;
-    int numberOfCoprimes;
-    struct Node* next;
-    struct Node* coprimes[COPRIME_ARRAY_LENGTH];
-} Node;
+    struct Root* nextRoot;
+    struct Coprime* nextCoprime;
+} Root;
+
+typedef struct Coprime {
+    int value;
+    struct Coprime* nextCoprime;
+} Coprime;
 
 /**
  <Returns the greatest common divisor for integers a and b.>
@@ -39,79 +41,80 @@ int gcd(int a, int b) {
     return 1;
 }
 
+/*
 // Finds the node that contains value n.
-Node* findNode(Node* head, int n) {
-    Node* node = head;
-    while(/*node != NULL*/ 1) {
+Root* findNode(Root* head, int n) {
+    Root* node = head;
+    while(1) {
         if ((*node).value == n) return node;
-        else node = (*node).next;
+        else node = (*node).nextRoot;
     }
-    // return NULL;
 }
+*/
 
-void findCoprimes(Node* head, Node* node, int max) {
-    // 1 is always coprime
-    (*node).coprimes[0] = head;
+void findCoprimes(Root* node, int max) {
+    Coprime* coprime;
+    Coprime* oldCoprime;
+
+    // 1 is always a coprime
+    coprime = (Coprime*) malloc(sizeof(Coprime));
+    (*coprime).value = 1;
+    (*node).nextCoprime = coprime;
 
     // loop through each potential coprime from 2 to max (inclusive)
     for (int coprimeCandidate = 2; coprimeCandidate < max + 1; coprimeCandidate++) {
-        // node and candidate are coprime if their gcd is 1
         if (gcd((*node).value, coprimeCandidate) == 1) {
-            // add candidate to list of coprimes of node
-            (*node).coprimes[(*node).numberOfCoprimes] = findNode(head, coprimeCandidate);
-            (*node).numberOfCoprimes++;
+            oldCoprime = coprime;
+            coprime = (Coprime*) malloc(sizeof(Coprime));
+            (*coprime).value = coprimeCandidate;
+            (*oldCoprime).nextCoprime = coprime;
         }
     }
+    (*coprime).nextCoprime = NULL;
 }
 
-Node* initializeNodes(int max) {
+Root* initializeNodes(int max) {
     printf("Initializing nodes...\n");
 
     // initialize head node which contains the number 1
-    Node* head = (Node*) malloc(sizeof(Node));
+    Root* head = (Root*) malloc(sizeof(Root));
     (*head).value = 1;
-    (*head).numberOfCoprimes = 1;
-    (*head).next = NULL;
-    for (int j = 0; j < COPRIME_ARRAY_LENGTH; j++) {
-        (*head).coprimes[j] = NULL;
-    }
+    (*head).nextRoot = NULL;
+    (*head).nextCoprime = NULL;
 
     // initialize body nodes
-    Node* nodeOld = head;
+    Root* nodeOld = head;
+    Root* node;
     for (int i = 2; i < max + 1; i++) {
-        Node* node = (Node*) malloc(sizeof(Node));
-        (*node).value = i;
-        (*node).numberOfCoprimes = 1;
-        
-        for (int j = 0; j < COPRIME_ARRAY_LENGTH; j++) {
-            (*node).coprimes[j] = NULL;
-        }
-
-        (*nodeOld).next = node;
+        node = (Root*) malloc(sizeof(Root));
+        (*node).value = i;       
+        (*nodeOld).nextRoot = node;
         nodeOld = node;
     }
-    (*nodeOld).next = NULL;
+    (*nodeOld).nextCoprime = NULL;
+    (*nodeOld).nextRoot = NULL;
 
     return head;
 }
 
-void linkCoprimes(Node* head, int max) {
-    Node* node = head;
-    for (int i = 0; i < max; i++) {
-        findCoprimes(head, node, max);
+void linkCoprimes(Root* head, int max) {
+    Root* node = head;
+    for (int i = 0; node != NULL; i++) {
+        findCoprimes(node, max);
         printf("Linked %d / %d nodes...\n", i + 1, max);
-        node = (*node).next;
+        node = (*node).nextRoot;
     }
 }
 
 // Generates graph of numbers that each link to their coprimes.
 // Returns a pointer to the head node of the graph (holding the number 1).
-Node* generateGraph(int max) {
-    Node* head = initializeNodes(max);
+Root* generateGraph(int max) {
+    Root* head = initializeNodes(max);
     linkCoprimes(head, max);
     return head;
 }
 
+/*
 void printNodeLegacy(Node* node, char* verbosity) {
     printf("%d\n", (*node).value);
     if (*verbosity == 'v') {
@@ -123,18 +126,22 @@ void printNodeLegacy(Node* node, char* verbosity) {
         }
     }
 }
+*/
 
-void printNode(Node* node, FILE* outFile) {
+void printNode(Root* node, FILE* outFile) {
     fprintf(outFile, "%d:", (*node).value);
-    for (int i = 0; i < (*node).numberOfCoprimes; i++)
-        fprintf(outFile, " %d", (*(*node).coprimes[i]).value);
+    Coprime* coprime = (*node).nextCoprime;
+    while (coprime != NULL) {
+        fprintf(outFile, " %d", (*coprime).value);
+        coprime = (*coprime).nextCoprime;
+    }
     fprintf(outFile, "\n");
 }
 
-void printGraph(Node* node, FILE* outFile) {
+void printGraph(Root* node, FILE* outFile) {
     while (node != NULL) {
         printNode(node, outFile);
-        node = (*node).next;
+        node = (*node).nextRoot;
     }
 }
 
@@ -150,7 +157,7 @@ int main() {
     printf("Enter max: ");
     scanf("%d", &max);
 
-    Node* head = generateGraph(max);
+    Root* head = generateGraph(max);
 
     printGraph(head, outFile);
 
