6277f500d4913d2e41d1be6ca8ac3e06af81a37a
Miles Roberts <milesrobertsmain@gmail.com>
2024-10-21 16:57:29 -0500

    added comments

--- Scripts/coprimeLinking/linkCoprimes.c
+++ Scripts/coprimeLinking/linkCoprimes.c
@@ -14,17 +14,21 @@ int main() {
     int max;
     char linkedCoprimesFilePath[64] = "../../Data/linkedCoprimes.txt";
     
+    // open file
     FILE *outFile = fopen(linkedCoprimesFilePath, "w");
     if (outFile == NULL) {
         printf("Error opening relative file path: '%s'\n", linkedCoprimesFilePath);
         return 1;
     }
 
+    // input max
     printf("Enter max: ");
     scanf("%d", &max);
 
+    // generate graph of integers and their coprimes
     Root* head = generateGraph(max);
 
+    // prints graph to the outFile
     printGraph(head, outFile);
 
     printf("Coprime graph written to: ../Data/linkedCoprimes.txt\n");
