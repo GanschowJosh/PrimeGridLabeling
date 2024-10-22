f64d2438e6bbb6c6d7c347435248f70d217f73bb
Miles Roberts <milesrobertsmain@gmail.com>
2024-10-22 15:42:53 -0500

    store test cases as 7z, extract when running CoprimeLinkerTest.sh

--- Tests/CoprimeLinkerTest.sh
+++ Tests/CoprimeLinkerTest.sh
@@ -1,16 +1,31 @@
 source=../CoprimeLinker/*.c
 binary=../CoprimeLinker/test
+testsZip=../Data/testCases.7z
+tests=./testCases
+passed=1
 
 gcc $source -o $binary
+
+7z x $testsZip > /dev/null
 
 for n in {1..50}; 
 do
     $binary $n 0
-    if [ "$(md5sum ../Data/testCases/linkedCoprimes-"$n".txt | awk '{print $1}')" == "$(md5sum ../Data/linkedCoprimes.txt | awk '{print $1}')" ];
+    if [ "$(md5sum $tests/linkedCoprimes-"$n".txt | awk '{print $1}')" == "$(md5sum ../Data/linkedCoprimes.txt | awk '{print $1}')" ];
         then echo "Passed case '$n'" 
     else 
         echo "Failed case '$n'"
+        $passed=0
     fi
 done
 
+
+rm -rf $tests
+
 rm $binary
+
+if [ $passed == 1 ];
+    then echo "All cases passed!"
+else
+    echo "One or more cases failed!"
+fi
