ec3bac829c7220dbb3f49214401564bc73071538
Miles Roberts <milesrobertsmain@gmail.com>
2024-10-22 15:46:09 -0500

    minor output change

--- Tests/CoprimeLinkerTest.sh
+++ Tests/CoprimeLinkerTest.sh
@@ -12,7 +12,7 @@ for n in {1..50};
 do
     $binary $n 0
     if [ "$(md5sum $tests/linkedCoprimes-"$n".txt | awk '{print $1}')" == "$(md5sum ../Data/linkedCoprimes.txt | awk '{print $1}')" ];
-        then echo "Passed case '$n'" 
+        then echo "Passed case for max = $n" 
     else 
         echo "Failed case '$n'"
         $passed=0
