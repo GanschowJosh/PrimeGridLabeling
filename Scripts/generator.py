2550f64eb3ec5edf66bc117c77dec9151dfdb7b2
Iurii Chmykhun <ichmykhun@gmail.com>
2024-10-22 21:32:31 -0500

    QoL generator.py updates

--- Scripts/generator.py
+++ Scripts/generator.py
@@ -1,34 +1,38 @@
-import time
-import GridPrimeLabeling
-import os
+from GridPrimeLabeling import generate_prime_grid
 from graph import print_2d_matrix_graph
+from time import perf_counter
+from os import makedirs
 
-# Range of NxN grids you'd like to try generating
+# Range of NxN grids to generate
 START_N = 1
-END_N = 61
+END_N = 20
 
-OUTPUT_FOLDER = "grids"
-SUCCESS_TRACKER_FILE = OUTPUT_FOLDER + "/_successful-grids.txt"
+OUTPUT_FOLDER = "grids/"  # ensure '/' at the end
+FILE_FORMAT = "{i}x{i}.txt"  # what the files will be named
 
-os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+# Ensure the output folder exists
+makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-# Meat & potatoes 
-sum_start = time.perf_counter()
+# Meat & potatoes of the generator
+total_start = perf_counter()
 for i in range(START_N, END_N + 1):
-    start = time.perf_counter()
-    prime_grid = GridPrimeLabeling.generate_prime_grid(i, i)
-    delta = round(time.perf_counter() - start, 4)
+    start = perf_counter()  # keep track of elapsed time
+    print(f"Generating {i}x{i} grid... ", end="")
 
+    # Perform the generation
+    prime_grid = generate_prime_grid(i, i)
+    delta = round(perf_counter() - start, 4)
+
+    # If valid grid exists, save it
     if prime_grid is not None:
-        print(f"Success! {i}x{i} grid took {delta} seconds.")
-        # Save successful grids in graphs directory
-        with open(f"{OUTPUT_FOLDER}/grid-{i}x{i}.txt", "w") as f:
+        print(f"Success! ({delta}s)")
+        # Save successful grids in output directory
+        with open(OUTPUT_FOLDER + FILE_FORMAT.format(i=i), "w") as f:
             f.write(
-                print_2d_matrix_graph(prime_grid, mute = True)
+                print_2d_matrix_graph(prime_grid, True)
             )
-            f.write("\n")
-        with open(SUCCESS_TRACKER_FILE, "a+") as f:
-            f.write(str(i) + "\n")
-    else:
-        print(f"FAILED. {i}x{i} grid took {delta} seconds.")
-print(f"All done! Total: {round(time.perf_counter() - sum_start, 2)}s.")
+    else:  # Grid generation failed
+        print(f"FAILED! ({delta}s).")
+
+total_time = round(perf_counter() - total_start, 2)
+print(f"All done!! ({total_time}s)")
